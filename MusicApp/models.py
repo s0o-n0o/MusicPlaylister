fb53a26a34477f6d125cce71fe156ab4a9a37924
s0o-n0o <sh8576278@gmail.com>
2022-10-18 23:51:51 +0900

    create UserModel

--- MusicApp/models.py
+++ MusicApp/models.py
@@ -1,3 +1,13 @@
+import email
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length=50)
+    email = models.EmailField()
+
+    def __str__(self) -> str:
+        return self.user_name
+
+    class Meta:
+        db_table = "user"
