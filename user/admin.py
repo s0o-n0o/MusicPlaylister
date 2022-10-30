82249f5df52868e19e485b9b1fbc97fca0c7a9d6
s0o-n0o <sh8576278@gmail.com>
2022-10-30 23:57:06 +0900

    create userform

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
+from .forms import UserCreationForm,UserChangeForm
 # Register your models here.
+
+Usre = get_user_model()
+
+class CustomizaUserAdmin(UserAdmin):
+    form = UserChangeForm #ユーザ編集画面で使うform
+    add_form = UserCreationForm #ユーザ作成画面で使うform
+#一覧画面で表示する
+    list_display = ('username','email')
+#ユーザ編集画面で表示する要素
+    fieldsets = (
+        ('ユーザ情報',{'fields':('usename','email','password')}),
+        
+    )
---
+++ user/forms.py
@@ -0,0 +1,38 @@
+from dataclasses import field
+from django import forms
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+
+User = get_user_model() #現在のアプリで使用しているモデルが返ってくる
+
+class UserCreationForm(forms.ModelForm):
+    password = forms.CharField(label = 'password',widget=forms.PasswordInput())
+    confirm_password = forms.CharField(label = 'password再入力',widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username','email','password')
+
+    def clean(self):
+        cleand_data = super().clean()
+        password = cleand_data.get('password')
+        confirm_password = cleand_data.get('comfirm_password')
+        if password != confirm_password:
+            raise ValidationError('パスワードが一致しません')
+    
+    def save(self):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data.get("password"))
+        user.save()
+        return user
+
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model= User
+        fields = ('username','email','password')
+
+    def clean_password(self):
+        return self.initial['password'] #initial➡既に登録されている値を返す
